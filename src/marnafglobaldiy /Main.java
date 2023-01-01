903ecf129b9243357d1c01d0ea850265b66a9265
marnafft <nafeesatua@gmail.com>
2023-01-01 13:54:47 +0100

    nigerias great

--- src/marnafglobaldiy /Main.java
+++ src/marnafglobaldiy /Main.java
@@ -1,9 +1,10 @@
-package marnafglobaldiy ;
+package marnafglobaldiy;
 
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       System.out.println("Hello, World!");
+    				System.out.println("i love Nigeria");
     }
     
 }
